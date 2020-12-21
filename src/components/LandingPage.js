fe098365d2d7341d47d50e94e4bdea4d2aaa759d
onTheDL <daniel.laserna@protonmail.com>
2020-12-20 21:14:59 -0500

    Add base LandingPage

---
+++ src/components/LandingPage.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import Lottie from 'react-lottie'
+import animationData from '../animations/landinganimation/data'
+import { makeStyles } from '@material-ui/core/styles'
+import {
+  Grid,
+  Button, 
+} from '@material-ui/core'
+import ButtonArrow from './ui/ButtonArrow'
+
+const useStyles = makeStyles(theme => ({
+
+}))
+
+export default function LandingPage() {
+  const classes = useStyles()
+  const defaultOptions = {
+    loop: true,
+    autoplay: true, 
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+  return (
+    <>
+      <Grid container direction="column">
+        <Grid item>
+          <Grid container direction="row" justify='flex-end'>
+
+            <Grid item>
+              <div>
+                Developing Awesome Software <br/>
+                D.A.S. is good!</div>
+              <Grid container>
+
+                <Grid item>
+                  <Button variant='contained'>Free Estimate</Button>
+                </Grid>
+
+                <Grid item>
+                  <Button variant='outlined'>
+                    Learn More
+                    <ButtonArrow width={15} height={15} fill='red' className />
+                  </Button>
+                </Grid>
+
+              </Grid>
+            </Grid>
+
+            <Grid item>
+              <Lottie options={defaultOptions} height={'100%'} width={'100%'} />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      
+    </>
+  )
+}
